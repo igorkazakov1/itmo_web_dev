1c90b0a0a22ec7928a83300d891f9089daee2513
Igor Kazakov <igorkazako1@gmail.com>
2018-11-05 22:20:20 +0300

    пока так

---
+++ html-exam/js/index.js
@@ -0,0 +1,7 @@
+sliderInit ('slider');
+
+sliderAdd('img/slider/1.jpg', 'alt текст', '#', 'What\'s Cooking?', 'Browse our gallery for dependably delicious recipes.' );
+sliderAdd('img/slider/2.jpg', 'alt текст');
+sliderAdd('img/slider/3.jpg', 'alt текст');
+sliderAdd('img/slider/4.jpg', 'alt текст');
+sliderRun();
